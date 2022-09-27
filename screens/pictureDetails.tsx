3768235627570f16e42349153afd84d6aad84140
satanrout <satyaranjanrout81@gmail.com>
2022-09-27 22:59:57 +0530

    added download option

--- screens/pictureDetails.tsx
+++ screens/pictureDetails.tsx
@@ -1,40 +1,86 @@
 import React, { useState } from "react";
 import { View, Text, Image } from "react-native";
-import { ActivityIndicator } from "react-native-paper";
+import { ActivityIndicator, Snackbar } from "react-native-paper";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { useQuery } from "react-query";
 import { useGetPhotoDetailsQuery } from "../redux/services/photos";
-import { FAB, Portal, Provider } from 'react-native-paper';
-import * as FileSystem from 'expo-file-system';
-import * as Permissions from 'expo-permissions';
-import * as MediaLibrary from 'expo-media-library';
+import { FAB, Portal, Provider } from "react-native-paper";
+import * as FileSystem from "expo-file-system";
+import * as Permissions from "expo-permissions";
+import * as MediaLibrary from "expo-media-library";
+
+interface Isnackbar {
+  label: string;
+  visible: boolean;
+  action: string;
+}
 
 export default function PictureDetails({ route, navigation }) {
   const { id } = route.params;
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   const { data, error, isLoading } = useGetPhotoDetailsQuery(id);
+  const [snackbar, setSnackbar] = useState<Isnackbar>({
+    label: "",
+    visible: false,
+    action: "",
+  });
 
   const onStateChange = () => setOpen(!open);
 
-  const downloadImage = () => {
-    const uri = data?.src?.original
-    const name = data?.url?.split('/')?.slice(-2,-1)?.[0] ||`img${data?.id}`
-    const fileUri = FileSystem.documentDirectory + name
-    FileSystem.downloadAsync(uri,fileUri).then(({uri}) => saveFile(uri) )
-  }
+  const downloadImage = async () => {
+    const uri: string = data?.src?.original;
+    const name: string =
+      data?.src?.original?.split("/")?.slice(-1)?.[0] || `img${data?.id}`;
+    const fileUri = FileSystem.documentDirectory + name;
+    const download = FileSystem.createDownloadResumable(
+      uri,
+      fileUri,
+      {},
+      getDownloadProgress
+    );
+    try {
+      const { uri } = await download.downloadAsync();
+      saveFile(uri);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
 
   const saveFile = async (fileUri: string) => {
-    console.log('hello')
-    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    console.log({status})
-    if(status === 'granted') {
-      const asset = await MediaLibrary.createAssetAsync(fileUri)
-        await MediaLibrary.createAlbumAsync("Download", asset, false)
+    console.log({ fileUri });
+    const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+    console.log({ status, fileUri });
+    if (status === "granted") {
+      const asset = await MediaLibrary.createAssetAsync(fileUri);
+      if (asset.filename) {
+        setSnackbar({
+          label: "Saved Sucessfully",
+          visible: true,
+          action: "Open",
+        });
+      }
+    } else {
+      setSnackbar({ label: "Unable to save", visible: true, action: "Close" });
     }
-  }
+  };
+
+  const getDownloadProgress = (downloadProgress) => {
+    const progress =
+      (downloadProgress.totalBytesWritten /
+        downloadProgress.totalBytesExpectedToWrite) *
+      100;
+    console.log(Math.trunc(progress));
+  };
 
   if (isLoading)
-    return <ActivityIndicator animating={true} style={{flex:1,alignItems:'center'}} size="large" color={Colors.red800} />;
+    return (
+      <ActivityIndicator
+        animating={true}
+        style={{ flex: 1, alignItems: "center" }}
+        size="large"
+        color={Colors.red800}
+      />
+    );
   if (data)
     return (
       <View>
@@ -43,33 +89,50 @@ export default function PictureDetails({ route, navigation }) {
           resizeMode="cover"
           source={{ uri: data?.src?.large2x }}
         />
-         <Portal>
-        <FAB.Group
-        visible
-          open={open}
-          icon={open ? 'calendar-today' : 'plus'}
-          actions={[
-            {
-              icon: 'share',
-              onPress: () => console.log('Pressed share'),
-            },
-            {
-              icon: 'download',
-              onPress: downloadImage,
-            },
-            {
-              icon: 'wallpaper',
-              onPress: () => console.log('Pressed wallpaper'),
-            },
-          ]}
-          onStateChange={onStateChange}
-          onPress={() => {
-            if (open) {
-              // do something if the speed dial is open
-            }
+        <View
+          style={{
+            backgroundColor: "blue",
+            height: 45,
+            width: "100%",
+            position: "absolute",
+            bottom: 0,
           }}
-        />
-      </Portal>
+        ></View>
+        <Portal>
+          <FAB.Group
+            visible
+            open={open}
+            icon={open ? "calendar-today" : "plus"}
+            actions={[
+              {
+                icon: "share",
+                onPress: () => console.log("Pressed share"),
+              },
+              {
+                icon: "download",
+                onPress: downloadImage,
+              },
+              {
+                icon: "wallpaper",
+                onPress: () => console.log("Pressed wallpaper"),
+              },
+            ]}
+            onStateChange={onStateChange}
+            onPress={() => {
+              if (open) {
+                // do something if the speed dial is open
+              }
+            }}
+          />
+        </Portal>
+        <Snackbar
+          visible={snackbar.visible}
+          onDismiss={() =>
+            setSnackbar({ label: "", visible: false, action: "" })
+          }
+        >
+          {snackbar.label}
+        </Snackbar>
       </View>
     );
 }
