0f71af83a014a31d9cdc4e058c740b0c2d0a0843
satanrout <satyaranjanrout81@gmail.com>
2022-10-06 06:57:05 +0530

    added search overlay

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Platform } from "react-native";
-import { Appbar, Searchbar } from "react-native-paper";
+import { View, Text, Platform, TextInput, StatusBar } from "react-native";
+import { Appbar, Modal, Portal, Searchbar } from "react-native-paper";
 import Test from "../screens/test";
 
 export default function Header({ back, navigation }) {
@@ -35,6 +35,44 @@ export default function Header({ back, navigation }) {
         />
       )}
       {!isSearch && <Appbar.Action icon={MORE_ICON} onPress={() => {}} />}
+      <Portal>
+        <Modal
+          visible={true}
+          onDismiss={() => {}}
+          contentContainerStyle={{
+            backgroundColor: "white",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              height: "100px",
+              width: "100%",
+              backgroundColor: "red",
+              // padding: StatusB.ar.currentHeight || 25,
+            }}
+          >
+            <View style={{ flex: 1 }}>
+              <Text>Search</Text>
+            </View>
+            <View style={{ flex: 4 }}>
+              <TextInput style={{ backgroundColor: "#f2f2f2" }} />
+            </View>
+            <View style={{ flex: 1 }}>
+              <Text>Search</Text>
+            </View>
+          </View>
+          <View style={{ backgroundColor: "green", height: "100%" }}>
+            <Text>Search</Text>
+          </View>
+        </Modal>
+      </Portal>
     </Appbar.Header>
   );
 }
